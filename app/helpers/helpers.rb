01772257c7ae422437c49c448949242111dbe74a
Vishal Sharma <vsh1819@gmail.com>
2018-07-12 17:07:31 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,19 @@
 class Helpers
+  
+  def self.current_user(session_hash)
+    id = session_hash[:user_id]
+    User.find(id)
+  end
+  
+  def self.is_logged_in?(session_hash)
+    id = session_hash[:user_id]
+    if id != nil
+      return true
+    else
+      return false
+    end
+  end
+    
+    
+    
 end
